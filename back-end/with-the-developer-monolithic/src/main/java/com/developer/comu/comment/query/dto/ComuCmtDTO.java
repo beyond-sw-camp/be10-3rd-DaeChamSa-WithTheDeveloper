70b3bcafdb9651899874539b3d7966f15817f049
Younggi Kim <galaxy138742@gmail.com>
2024-10-25 18:26:00 +0900

    feat: 커뮤니티 게시글, 댓글 조회 데이터 값 변경

--- back-end/with-the-developer-monolithic/src/main/java/com/developer/comu/comment/query/dto/ComuCmtDTO.java
+++ back-end/with-the-developer-monolithic/src/main/java/com/developer/comu/comment/query/dto/ComuCmtDTO.java
@@ -2,8 +2,12 @@ package com.developer.comu.comment.query.dto;
 
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Data
 public class ComuCmtDTO {
     private Long comuCmtCode;
     private String comuCmtContent;
+    private String userNick;
+    private LocalDateTime createdDate;
 }
